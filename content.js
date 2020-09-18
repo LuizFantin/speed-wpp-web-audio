c12c6974516a5f668f5c399a75be1552e7cbe4ee
Luiz Fantin <fantin_luiz@outlook.com>
2020-09-18 00:44:43 -0300

    Active colors in buttons

--- content.js
+++ content.js
@@ -2,7 +2,6 @@
 const interval = setInterval(()=>{
     const header = document.querySelector("._1QUKR") 
     if(header){
-        console.log(header)
         clearInterval(interval)
 
         const button = document.createElement("button")
@@ -12,20 +11,28 @@ const interval = setInterval(()=>{
         button2.innerHTML = "2x"
         button2.classList.add("twoTimesButton")
 
+        button.style.color = "#4169e1"
+
         button.addEventListener("click", ()=>{
             const audios = document.querySelectorAll("audio")
             audios.forEach((audio)=>{
                 audio.playbackRate = 1
             })
+            button.style.color = "#4169e1"
+            button2.style.color = "#d3d3d3"
+
         })
         button2.addEventListener("click", ()=>{
             const audios = document.querySelectorAll("audio")
             audios.forEach((audio)=>{
                 audio.playbackRate = 2
             })
+            button2.style.color = "#4169e1"
+            button.style.color = "#d3d3d3"
         })
 
         header.appendChild(button)
         header.appendChild(button2)
     }
 },1000)
+
